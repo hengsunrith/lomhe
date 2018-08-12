0442c02f43ed0c9a95807508fd35ee1c423eb9b4
sunrith <sunrith2012@gmail.com>
2018-08-12 10:54:36 +0700

    static page is done.

--- static_page/urls.py
+++ static_page/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('sign_in/', views.sign_in, name='sign_in'),
+    path('sign_up/', views.sign_up, name='sign_up'),
+    path('profile/', views.profile, name='profile'),
+    path('post_detial/', views.post_detial, name='post_detial'),
+    path('forget_pwd/', views.forget_pwd, name='forget_pwd'),
 ]
--- static_page/views.py
+++ static_page/views.py
@@ -1,6 +1,28 @@
-from django.shortcuts import render
+from django.shortcuts import render_to_response, render
 from django.http import HttpResponse
 
-# Create your views here.
-def index(request):
-  return HttpResponse("Hello, world. You're at the polls index.")
+
+def home(request):
+  # return render_to_response('templates/base.html')
+  return render(request, 'static_page/home.html', {})
+
+def about(request):
+  return render(request, 'static_page/about.html')
+
+def contact(request):
+  return render(request, 'static_page/contact.html')
+
+def sign_in(request):
+  return render(request, 'static_page/registrations/sign_in.html')
+
+def sign_up(request):
+  return render(request, 'static_page/registrations/sign_up.html')
+
+def profile(request):
+  return render(request, 'static_page/registrations/profile.html')
+
+def post_detial(request):
+  return render(request, 'static_page/post_detial.html')
+
+def forget_pwd(request):
+  return render(request, 'static_page/registrations/forget_pwd.html')
